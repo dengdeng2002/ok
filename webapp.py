62d48236590ab2ab7f5e413aafbfd9c5693b1c42
dengdeng2002 <dengdeng2002@gmail.com>
2025-06-18 20:04:39 +0800

    one commit

---
+++ webapp.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import shap
+import joblib
+import xgboost
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import lightgbm
+from pathlib import Path
+
+def main():
+    best_model = joblib.load('best_model_lgbm.pkl')
+
+    class Subject:
+        def __init__(self, AGE, BMI, RACE, C4_0):
+            self.RACE = RACE
+            self.BMI = BMI
+            self.AGE = AGE
+            self.C4_0 = C4_0
+
+
+        def make_predict(self):
+            subject_data = {
+                "RACE": [self.RACE],
+                "BMI": [self.BMI],
+                "AGE": [self.AGE],
+                "C4_0": [self.C4_0]
+            }
+
+            # Create a DataFrame
+            df_subject = pd.DataFrame(subject_data)
+
+            # Make the prediction
+            prediction = best_model.predict_proba(df_subject)[:, 1]
+            adjusted_prediction = np.round(prediction * 100, 2)
+            st.write(f"""
+                <div class='all'>
+                    <p style='text-align: center; font-size: 20px;'>
+                        <b>The model predicts the prevalence of preserved ratio impaired spirometry is {adjusted_prediction} %</b>
+                    </p>
+                </div>
+            """, unsafe_allow_html=True)
+
+            explainer = shap.Explainer(best_model)
+            shap_values = explainer.shap_values(df_subject)
+            # 力图
+            shap.force_plot(explainer.expected_value[1], shap_values[1][0, :], df_subject.iloc[0, :], matplotlib=True)
+            # 瀑布图
+            # ex = shap.Explanation(shap_values[1][0, :], explainer.expected_value[1], df_subject.iloc[0, :])
+            # shap.waterfall_plot(ex)
+            st.pyplot(plt.gcf())
+
+    st.set_page_config(page_title='the prevalence of preserved ratio impaired spirometry')
+    st.markdown(f"""
+                <div class='all'>
+                    <h1 style='text-align: center;'>Predicting the prevalence of preserved ratio impaired spirometry</h1>
+                    <p class='intro'></p>
+                </div>
+                """, unsafe_allow_html=True)
+    st.markdown("""
+    <div style='font-size:14px; line-height:1.5; color:#6c757d;'>
+        <b>Acknowledgments</b><br>
+        Developed by: Deng <i>et&nbsp;al.</i><br>
+        Contact: dengcy0758@163.com<br><br>
+
+        <b>Terms of Use</b><br>
+        This tool is for research use only.&nbsp;It is not intended for clinical diagnosis or treatment decision-making.<br>
+        The model was trained on individuals aged&nbsp;20–79&nbsp;years.<br><br>
+
+        <b>Data Privacy</b><br>
+        No input data is stored on the server.&nbsp;All inputs are processed only temporarily and not used for any other purposes.
+    </div>
+    """, unsafe_allow_html=True)
+
+    RACE = st.selectbox("Race (Mexican American = 1, Other Hispanic = 2, Non-Hispanic White = 3, Non-Hispanic Black = 4, Other race = 5)", [1,2,3,4,5], index=3)
+    BMI = st.number_input("BMI (kg/m^2)", value=24.51)
+    AGE = st.number_input("Age (years)", value=48)
+    C4_0 =st.number_input("dietary C4:0 fatty acid intake (g/day)", value=2.411)
+
+
+    if st.button(label="Submit"):
+        user = Subject(AGE, BMI, RACE, C4_0)
+        user.make_predict()
+
+    if st.button("Reset all inputs"):
+        st.session_state.clear()        
+        st.experimental_rerun()  
+main()
